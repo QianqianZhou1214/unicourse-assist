40bbcbd070fd7859339e438147d4a529b7156d80
Qianqian Zhou <qqzh1214@gmail.com>
2025-11-22 14:53:46 +0100

    fixed the file path problem

--- ai-py/app/ingest/ingest.py
+++ ai-py/app/ingest/ingest.py
@@ -4,6 +4,18 @@ from chunk import chunk_text
 from embed import embed_texts
 import chromadb
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ROOT_DIR = os.path.abspath(os.path.join(THIS_DIR, "..", ".."))
+DATA_DIR = os.path.join(ROOT_DIR, "app","data")
+VECTOR_DIR = os.path.join(ROOT_DIR, "app", "vector_store")
+os.makedirs(VECTOR_DIR, exist_ok=True)
+
+print("ROOT_DIR =", ROOT_DIR)
+print("DATA_DIR =", DATA_DIR)
+print("VECTOR_DIR =", VECTOR_DIR)
+
+
 def ingest_file(path: str, collection):
     """
     Selects the appropriate extractor based on file type.
@@ -34,11 +46,12 @@ def ingest_file(path: str, collection):
     print(f"[OK] Ingested {path} with {len(chunks)} chunks.")
 
 
-def run_ingest(data_dir="data"):
-    chroma = chromadb.PersistentClient(path="../vertor_store")
+def run_ingest(data_dir=DATA_DIR):
+    print("Using data directory:", data_dir)
+    chroma = chromadb.PersistentClient(path=VECTOR_DIR)
 
     collection = chroma.get_or_create_collection(
-        name="courses:," 
+        name="courses",
         metadata={"hnsw:space": "cosine"}
     )
 
